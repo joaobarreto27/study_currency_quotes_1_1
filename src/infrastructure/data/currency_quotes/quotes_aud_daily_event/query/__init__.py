ff113ec3fb6aec6241b8afbabedf6349d109b538
joaobarreto27 <joao.vito1951@gmail.com>
2025-10-08 21:18:08 -0300

    feat: Adicionando processo de extração para a feature 'quote-aud'.

---
+++ src/infrastructure/data/currency_quotes/quotes_aud_daily_event/query/__init__.py
@@ -0,0 +1,7 @@
+"""Pacote de comandos para eventos diários de cotação AUD."""
+
+from .quotes_aud_daily_event_query_repository import (
+    QuotesAudDailyEventQueryRepository as QuotesAudDailyEventQueryRepository,
+)
+
+__all__ = ["QuotesAudDailyEventQueryRepository"]
